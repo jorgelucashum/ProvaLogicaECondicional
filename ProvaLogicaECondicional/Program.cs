e4de755b7913480303a8816b99de2be3810a3271
jorgeucas lucas <jorge_dofus1@hotmail.com>
2021-03-01 17:33:15 -0300

    finalizado 1

--- ProvaLogicaECondicional/Program.cs
+++ ProvaLogicaECondicional/Program.cs
@@ -49,7 +49,7 @@ namespace ProvaLogicaECondicional
             for (int cont = 0; cont < 2; cont++)
             {
 
-                Console.WriteLine("\n3)Digite o codigo do alimento e a quantidade do alimento desejado \n1-Cachorro Quente\n2-X Salada\n3-X Bacon\n4-Torrada simples\n5-Refrigerente");
+                Console.WriteLine("\n3)Digite o codigo do alimento e a quantidade do alimento desejado. Exemplo: 3 2 \n1-Cachorro Quente\n2-X Salada\n3-X Bacon\n4-Torrada simples\n5-Refrigerente\n");
                 string[] entradas = Console.ReadLine().Split();
                 float l1 = float.Parse(entradas[0]), q1 = float.Parse(entradas[1]);
                 float lanche;
@@ -85,22 +85,61 @@ namespace ProvaLogicaECondicional
 
             // Exercicio - 4
 
-            Console.WriteLine("\n4)Digite os valores de a, b e c em sequencia para realizar a equação do 2º grau: ");
+            for (int cont = 0; cont < 4; cont++) 
+                { 
+            Console.WriteLine("\n4)Digite os valores de a, b e c em sequencia para realizar a equação do 2º grau. Exemplo: 10 20,1 5,1 ");
             string[] abc = Console.ReadLine().Split();
             float a = float.Parse(abc[0]), b = float.Parse(abc[1]), c = float.Parse(abc[2]);
             float delta = (b * b) - (4 * a * c);
+                if (delta > 0 && a != 0) 
+                { 
             double x1 = (-(b) + Math.Sqrt(delta)) / (2 * a);
             double x2 = (-(b) - Math.Sqrt(delta)) / (2 * a);
             Console.WriteLine("X1 = " + Math.Round(x1, 5) + " X2 = " + Math.Round(x2, 5));
+                } else
+                {
+                    Console.WriteLine("Impossível calcular");
+                }
+            }
 
 
+            // Exercicio - 5;
+            for (int cont = 0; cont == 0;)
+            {
+                Console.WriteLine("\n5) Digite a senha: ");
+                int senha = int.Parse(Console.ReadLine());
+                if (senha == 2002) {
+                    Console.WriteLine("Acesso permitido\n");
+                    cont = + 1;
+                } else
+                {
+                    Console.WriteLine("Senha Invalida");
+                }
+            }
 
 
+            // Exercicio - 6
+            Console.WriteLine("Digite a quantidade de numeros a serem lidos");
+            int n = int.Parse(Console.ReadLine());
+            int[] x = new int[n];
+            int inIntervalo = 0;
+            int outIntervalo = 0;
+            for (int cont = 0; cont < x.Length; cont++)
+            {
+                Console.WriteLine("Digite um número: ");
+                x[cont] = int.Parse(Console.ReadLine());
+                if (x[cont] >9 && x[cont] < 21)
+                {
+                     inIntervalo = inIntervalo + 1; 
+                } else
+                {
+                    outIntervalo = outIntervalo + 1;
+                }
+            }
+            Console.WriteLine("\nNúmeros dentro do intervalo entre 10 e 20: " + inIntervalo);
+            Console.WriteLine("Números fora do intervalo entre 10 e 20: " + outIntervalo);
 
             Console.ReadLine();
         }
-
-       
-
     }
 }
